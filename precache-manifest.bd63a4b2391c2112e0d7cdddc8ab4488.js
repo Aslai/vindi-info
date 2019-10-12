184fae86ba82baeae5aa57e052b9627c24fd8b56
kaslai <kaslai@kaslai.com>
2019-10-12 14:54:03 -0700

    Updates

---
+++ precache-manifest.bd63a4b2391c2112e0d7cdddc8ab4488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2ae38fbc0da3997e3f3a5edb92e9a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acc216743956c2a958c6",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "9e20e05efe9b77d7a9be",
+    "url": "/static/css/main.58a0406e.chunk.css"
+  },
+  {
+    "revision": "acc216743956c2a958c6",
+    "url": "/static/js/2.12846fd8.chunk.js"
+  },
+  {
+    "revision": "9e20e05efe9b77d7a9be",
+    "url": "/static/js/main.04006348.chunk.js"
+  },
+  {
+    "revision": "16c44865936678f08710",
+    "url": "/static/js/runtime-main.c90aec88.js"
+  }
+]);
