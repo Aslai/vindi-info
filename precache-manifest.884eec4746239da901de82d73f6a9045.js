91d4fd7a9c6f7742c7ad68b8c9e06caa3cb3a2ed
kaslai <kaslai@kaslai.com>
2019-10-13 03:09:21 -0700

    Updates

---
+++ precache-manifest.884eec4746239da901de82d73f6a9045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c93984bb20ed093e0b7b1edce9f6e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467b360aff48f0405651",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "242cecc00c249dc8a59f",
+    "url": "/static/css/main.36d435e2.chunk.css"
+  },
+  {
+    "revision": "467b360aff48f0405651",
+    "url": "/static/js/2.74457cdd.chunk.js"
+  },
+  {
+    "revision": "242cecc00c249dc8a59f",
+    "url": "/static/js/main.7894d791.chunk.js"
+  },
+  {
+    "revision": "16c44865936678f08710",
+    "url": "/static/js/runtime-main.c90aec88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33f73c0e9d1fb4575d0423057f810eb7.js"
+  "/precache-manifest.884eec4746239da901de82d73f6a9045.js"
 );
 
 self.addEventListener('message', (event) => {
